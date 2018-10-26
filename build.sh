9503936528d1f0b69b52e2a831f01d6dbeb7255e
Geoffrey Tipton <tipton@mysecretroom.com>
2018-10-26 17:34:35 +0800

    ..

--- build.sh
+++ build.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)
+                machine=Mac
+                export LC_ALL=en_US.UTF-8
+                export LANG=en_US.UTF-8
+    ;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+make livehtml
+echo ${machine}
